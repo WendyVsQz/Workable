9634f3f4ac78571999c5bb13b46b421f5141c1c4
kh.saint44 <119610043+44-khowell@users.noreply.github.com>
2023-03-23 21:02:30 +0000

    WIP: exported GridWrapper search bar styling to own styles file

--- src/components/GridWrapper/GridWrapper.jsx
+++ src/components/GridWrapper/GridWrapper.jsx
@@ -1,21 +1,13 @@
 import React from 'react'
 import Grid from '@mui/material/Grid';
+import { gridWrapperStyles } from './styles.js';
 
 const GridWrapper = ({children}) => {
-    const gridWrapperStyles = {
-            marginLeft: '200px', 
-            marginRight: '200px', 
-            backgroundColor: '#eaeff1', 
-            padding: '250px 32px', 
-            minHeight: 'calc(100vh - 50px)', 
-            position: 'relative',
-    };
 
     return (
         <Grid item xs={12} sx={gridWrapperStyles}>
             {children}
         </Grid>
-
     )
 }
 
---
+++ src/components/GridWrapper/styles.js
@@ -0,0 +1,9 @@
+
+export const gridWrapperStyles = {
+    marginLeft: '200px', 
+    marginRight: '200px', 
+    backgroundColor: '#eaeff1', 
+    padding: '250px 32px', 
+    minHeight: 'calc(100vh - 50px)', 
+    position: 'relative',
+};
