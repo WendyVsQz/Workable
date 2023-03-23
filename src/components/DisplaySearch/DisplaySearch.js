c4f19673bee0a057f45559e1b3736c39b7d6f32f
kh.saint44 <119610043+44-khowell@users.noreply.github.com>
2023-03-23 21:24:54 +0000

    WIP: exported DisplaySearch card styling to own styles file

--- src/components/DisplaySearch/DisplaySearch.js
+++ src/components/DisplaySearch/DisplaySearch.js
@@ -5,6 +5,7 @@ import SearchBar from '../Searchbar/SearchBar';
 import Box from '@mui/material/Box';
 import CommonButton from '../CommonButton/CommonButton';
 import GridWrapper from '../GridWrapper/GridWrapper';
+import { cardheaderStyles } from './styles';
 
 const DisplaySearch = () => {
 
@@ -17,25 +18,9 @@ const DisplaySearch = () => {
             console.log('click')
         };
 
-        const headerStyles = {
-            wrapper: {
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-                paddingLeft: '20px',
-                paddingRight: '20px',
-                height: '65px',
-                backgroundColor: '#f5f5f5',
-                borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
-            },
-            searchButton: {
-                fontSize: '1.05rem',
-                borderRadius: '2.05rem',
-            },
-        }
 
         return (
-            <Box sx={headerStyles.wrapper}>
+            <Box sx={cardheaderStyles.wrapper}>
                 <SearchBar
                     placeholder='Search your next job...'
                     onChange={(event) => handleChange(event.target.value)}
@@ -46,7 +31,7 @@ const DisplaySearch = () => {
                         variant="contained"
                         onClick={searchBtn}
                         size="large"
-                        sx={headerStyles.searchButton}
+                        sx={cardheaderStyles.searchButton}
                     >
                         Search
                     </CommonButton>
---
+++ src/components/DisplaySearch/styles.js
@@ -0,0 +1,22 @@
+
+export const cardheaderStyles = {
+    wrapper: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingLeft: '20px',
+        paddingRight: '20px',
+        height: '65px',
+        backgroundColor: '#f5f5f5',
+        borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+    },
+    searchButton: {
+        fontSize: '1.05rem',
+        borderRadius: '2.05rem',
+    },
+}
+
+
+
+
+
