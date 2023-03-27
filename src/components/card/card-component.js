7005d746a3d08e1887d9cf7e8800e368e1d99571
laurabbity915 <laura_yuen915@outlook.com>
2023-03-27 21:04:19 +0100

    added wish button to card

--- src/components/card/card-component.js
+++ src/components/card/card-component.js
@@ -1,15 +1,67 @@
-import { Card, Button } from "@mui/material";
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import { Button, Card, Box } from "@mui/material";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import * as React from "react";
+import { decode } from 'html-entities';
+import React, { useEffect, useState } from "react";
+import { getFromStorage, saveToStorage } from '../../utils/storage-utils';
 import "./card-component.css";
 
 export function CardComponent(props) {
+
+  const BOOKMARK = "bookmarked";
+
+  const [bookMarked, setBookMarked] = useState(false);
+
+  const handleBookMark = (job) => {
+    // get the existing book marked list
+    let temp = getFromStorage(BOOKMARK);
+    let marked = new Map();
+
+    if (temp != null) {
+      marked = new Map(Object.entries(temp));
+    }
+    // check job exist in the local storage or not
+    if (marked.get(job.jobId.toString()) != null) {
+      // remove job if exist
+      marked.delete(job.jobId.toString());
+      setBookMarked(false);
+    } else {
+      // add job to book mark if not exist
+      marked.set(job.jobId, job);
+      setBookMarked(true);
+    }
+
+    saveToStorage(BOOKMARK, Object.fromEntries(marked));
+  }
+
+  useEffect(() => {
+    let temp = getFromStorage(BOOKMARK);
+    let marked = new Map(Object.entries(temp));
+    setBookMarked(marked.get(props.job.jobId.toString()) ? true : false);
+  }, [props.job]);
+
   return (
     <>
-      <Card variant="outlined" className="custom-card-layout">
+      <Card variant="outlined" className="custom-card-layout" key={props.job.jobId}>
         <React.Fragment>
+          <Box hidden={!props.showBookMark}>
+            <BookmarkBorderIcon
+              hidden={bookMarked}
+              className="book-mark-button"
+              onClick={() => {
+                handleBookMark(props.job);
+              }} />
+
+            <BookmarkIcon
+              hidden={!bookMarked}
+              className="book-mark-button-checked"
+              onClick={() => {
+                handleBookMark(props.job);
+              }} />
+          </Box>
           <CardContent>
             <Typography
               variant="h4"
@@ -22,7 +74,7 @@ export function CardComponent(props) {
             <Typography component="div" className="custom-card-text">
               <b>Job Description: </b>
               <br />
-              {props.job.jobDescription}
+              {decode(props.job.jobDescription)}
             </Typography>
             <Typography
               sx={{ mb: 1.5 }}
@@ -34,8 +86,7 @@ export function CardComponent(props) {
             </Typography>
           </CardContent>
           <CardActions className="detials-button-right">
-            <Button size="small">Details</Button>
-            {/* onClick={() => props.handleOnClick(props.project)} */}
+            <Button size="small" onClick={() => window.open(props.job.jobUrl, '_blank')}>More</Button>
           </CardActions>
         </React.Fragment>
       </Card>
